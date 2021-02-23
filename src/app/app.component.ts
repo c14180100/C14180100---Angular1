ec470458bae84f6e7ad15af62e913ae427735ec3
c14180100 <c14180100@john.petra.ac.id>
2021-02-24 01:37:08 +0700

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,98 +1,93 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  function hitung(){
-			var input = document.getElementById("input").value;
-			var inputs = input.split(",");
-			var row = parseInt(inputs[0]);
-			var col = parseInt(inputs[1]);
+export class AppComponent {
+  input: String;
+  giliran: String = "Giliran 1";
+  jawaban: String = "*****<br>*****<br>*****<br>*****<br>*****";
+  pemenang: String;
 
-			var gil = document.getElementById("giliran").innerHTML.split(" ");
-			var giliran = gil[1];
+  hitung() {
+    var inputs = this.input.split(",");
+    var row = parseInt(inputs[0]);
+    var col = parseInt(inputs[1]);
 
-			var jawaban = document.getElementById("jawaban").innerHTML;
+    var gil = this.giliran.split(" ");
+    var giliran = gil[1];
 
-			var splitJawaban = jawaban.split("<br>");
+    var splitJawaban = this.jawaban.split("<br>");
 
-			var proses = new Array();
-			for(var i = 0; i < 5; i++){
-				proses[i] = splitJawaban[i].split("");
+    var proses = new Array();
+    for (var i = 0; i < 5; i++) {
+      proses[i] = splitJawaban[i].split("");
+    }
 
+    proses[row - 1][col - 1] = giliran;
 
-			}
+    //cek kondisi menang
+    var menang = "0";
+    var counter = 0;
 
-			proses[row-1][col-1] = giliran;
+    //horizontal
+    for (var i = 0; i < 5; i++) {
+      counter = 0;
+      for (var j = 0; j < 5; j++) {
+        if (proses[i][j] == giliran) {
+          counter++;
+        } else {
+          counter = 0;
+        }
+      }
+      if (counter >= 4) {
+        menang = giliran;
+      }
+    }
 
-			//cek kondisi menang
-			var menang = "0";
-			var counter = 0;
+    //vertikal
+    if (menang == "0") {
+      for (var i = 0; i < 5; i++) {
+        counter = 0;
+        for (var j = 0; j < 5; j++) {
+          if (proses[j][i] == giliran) {
+            counter++;
+          } else {
+            counter = 0;
+          }
+        }
+        if (counter >= 4) {
+          menang = giliran;
+        }
+      }
+    }
 
-			//horizontal
-			for(var i = 0; i < 5; i++){
-				counter = 0;
-				for (var j = 0; j < 5; j++){
-					if(proses[i][j] == giliran){
-						counter++;
-					}
-					else{
-						counter = 0;
-					}
-				}
-				if(counter >= 4){
-					menang = giliran;
-				}
-			}
+    //gabungin lagi arraynya jadi string
 
-			//vertikal
-			if(menang == "0"){
-				for(var i = 0; i < 5; i++){
-					counter = 0;
-					for (var j = 0; j < 5; j++){
-						if(proses[j][i] == giliran){
-							counter++;
-						}
-						else{
-							counter = 0;
-						}
-					}
-					if(counter >= 4){
-						menang = giliran;
-					}
-				}
-			}
+    var gabung = new Array();
 
-			//gabungin lagi arraynya jadi string
+    for (var i = 0; i < 5 - 1; i++) {
+      proses[i].push("<br>");
+      gabung[i] = proses[i].join("");
+    }
+    gabung[5 - 1] = proses[5 - 1].join("");
 
-			var gabung = new Array();
+    var jawabanFix = gabung.join("");
+    this.jawaban = jawabanFix;
 
-			for(var i = 0; i < 5-1; i++){
-				proses[i].push("<br>");
-				gabung[i] = proses[i].join("");
-			}
-			gabung[5-1] = proses[5-1].join(""); 
+    if (menang != "0") {
+      this.pemenang =
+        "Pemenangnya adalah " + giliran + " !";
+    } else {
+      if (giliran == "1") {
+        giliran = "2";
+      } else {
+        giliran = "1";
+      }
 
-			var jawabanFix = gabung.join("");
-			document.getElementById("jawaban").innerHTML = jawabanFix;
-
-			
-
-			if(menang != "0"){
-				document.getElementById("pemenang").innerHTML = "Pemenangnya adalah " + giliran + " !"; 
-			}
-			else{
-				if(giliran == "1"){
-					giliran = "2";
-				}
-				else{
-					giliran = "1";
-				}
-
-				document.getElementById("giliran").innerHTML = "Giliran " + giliran;
-			}
-		}
+      this.giliran = "Giliran " + giliran;
+    }
+  }
 }
